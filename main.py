e2c343132c5e5df0359c9c21f3f226d64346138c
Marcos <muf1002@alu.ubu.es>
2024-05-01 23:13:08 +0200

    Realización de todos los cambios restantes

--- main.py
+++ main.py
@@ -14,9 +14,16 @@ migrate = Migrate(app, db)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True)
+    cod_empresa = db.Column(db.Integer, unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False) 
     password_hash = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    nombre = db.Column(db.String(50), nullable=True)
+    apellido = db.Column(db.String(50), nullable=True)
+    genero = db.Column(db.String(50), nullable=True)
+    edad = db.Column(db.Integer, nullable=True)
+    rol = db.Column(db.String(50), nullable=True)
+    CampoPr = db.Column(db.String(50), nullable=True)
+
 
     def set_password(self, password):
         """
@@ -54,17 +61,22 @@ def registro():
     Gestiona el registro de nuevos usuarios.
     """
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        cod_empresa = request.form['cod_empresa']
         email = request.form['email']
+        password = request.form['password'] 
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        genero = request.form['genero']
+        edad = request.form['edad']
+        rol = request.form['rol']
+        CampoPr = None
 
-        existing_user = User.query.filter_by(username=username).first()
+        existing_user = User.query.filter_by(email=email).first()
         if existing_user:
-            return render_template('registro.html', error='El usuario ya existe')
+            return render_template('registro.html', error='El correo ya existe')
 
-        new_user = User(id=1,username=username, email=email)
+        new_user = User( cod_empresa=cod_empresa, email=email, nombre=nombre, apellido=apellido, genero=genero, edad=edad, rol=rol, CampoPr= CampoPr)
         new_user.set_password(password)
-        
 
         db.session.add(new_user)
         db.session.commit()
@@ -76,43 +88,91 @@ def registro():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(email=email).first()
         if user and user.check_password(password):
+            nombre=user.nombre
             user_id = str(uuid.uuid4())
             session['user_id'] = user_id
-            session['username'] = username
+            session['nombre'] = nombre
             return redirect(url_for('inicio'))
         else:
             return render_template('login.html', error='Credenciales incorrectas')
     return render_template('login.html')
 
+@app.route('/cambiarcontraseña', methods=['GET', 'POST'])
+def cambiar_contraseña():
+    if request.method == 'POST':
+        email = request.form['email']
+        new_password = request.form['new_password']
+        confirm_password = request.form['confirm_password']
+
+        if new_password != confirm_password:
+            return render_template('cambiarContraseña.html', error='Las contraseñas no coinciden')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.set_password(new_password)
+            db.session.commit()
+            return redirect(url_for('login'))
+        else:
+            return render_template('cambiarContraseña.html', error='No se encontró ningún usuario con ese correo electrónico')
+
+    return render_template('cambiarContraseña.html')
+
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
-    session.pop('username', None)
+    session.pop('nombre', None)
     return redirect(url_for('inicio'))
+
+@app.route("/esta_logeado")
+def esta_logeado():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('datos_invitado'))
+    else:
+        return redirect(url_for('test_page', page=1))
+    
+@app.route("/invitado",  methods=['GET', 'POST'])
+def datos_invitado():
+    if request.method == 'POST':
+        cod_empresa = None
+        nombre='invitado'
+        apellido = None
+        edad = request.form['edad']
+        rol = request.form['rol']
+        genero = request.form['genero']
+        CampoPr = request.form['CampoPr']
+
+        count = User.query.filter_by(nombre='invitado').count()
+        if count == 0:
+            invitado = User(cod_empresa=cod_empresa, email='example@gmail.com', nombre=nombre, apellido=apellido, genero=genero, edad=edad, rol=rol, CampoPr= CampoPr)
+            invitado.set_password('soyelinvitado') 
+            db.session.add(invitado)
+        else:
+            invitado = User.query.filter_by(nombre='invitado').first()
+            invitado.genero = genero
+            invitado.edad = edad
+            invitado.rol = rol
+            invitado.CampoPr = CampoPr
+
+        db.session.commit()
+        user_id = str(uuid.uuid4())
+        session['user_id'] = user_id
+        session['nombre'] = 'invitado'  
+        return redirect(url_for('test_page', page=1))
+    
+    return render_template('datosInvitado.html')
 
 @app.route("/test/p<path:page>", methods=['GET', 'POST'])
 def test_page(page):
     """
     Renderiza la página del test según el parámetro 'page'.
     """
-
-    user_id = session.get('user_id')
-    if not user_id:
-        count = User.query.filter_by(username='invitado').count()
-        if count == 0:
-            invitado = User(id=1,username='invitado',email='example@gamil.com')
-            invitado.set_password('soyelinvitado') 
-            db.session.add(invitado)
-            db.session.commit()
     
-        user_id = str(uuid.uuid4())
-        session['user_id'] = user_id
-        session['username'] = 'invitado'        
-    
+    user_id = session.get('user_id')        
     user_data = session.setdefault(user_id, {'page_counter': 1, 'total_valor': 0})
     if user_data['page_counter'] == 5:
         user_data['page_counter'] = 1
@@ -165,7 +225,7 @@ def test_page(page):
 def resultado_page():
     total_valor = session.get('total_valor', 0)
 
-    resultado = Resultados(usuario=session.get('username'), fecha=datetime.now(), puntuacion=total_valor)
+    resultado = Resultados(usuario=session.get('nombre'), fecha=datetime.now(), puntuacion=total_valor)
     db.session.add(resultado)
     db.session.commit()
     return render_template('resultado.html', total_valor=total_valor)
